ca3ed092eb03f5311ac34456ca09a3b4ba7c2345
ItVal <valentinhonvj1@gmail.com>
2022-08-12 10:37:27 +0200

    feat :Converting Seconds to Minutes using Moment.js

--- src/Components/Break.jsx
+++ src/Components/Break.jsx
@@ -1,13 +1,15 @@
+import moment from "moment";
 import React, { useState } from "react";
 
 
 
 const Break = () => {
+    // initialisation de la durée de la pause à l'aide de react state (et useState)
     const [
         breakLengthInSeconds,
         setBreakLengthInSeconds
     ] = useState(300);
-
+// ajout bouton plus et moins avec des gestionnaires d'événements de clic
     const decrementBreakLengthByOneMinute = () => {
         const newBreakLengthInSeconds =
             breakLengthInSeconds - 60;
@@ -25,11 +27,12 @@ const Break = () => {
         setBreakLengthInSeconds(
             breakLengthInSeconds + 60
         );
-
+        // conversion secondes en minutes à l'aide de moment.js
+    const breakLengthInMinutes = moment.duration(breakLengthInSeconds, "s").minutes(); 
 return (
     <div>
         <p id="break-label">Break</p>
-        <p id="break-length">{breakLengthInSeconds}</p>
+        <p id="break-length">{breakLengthInMinutes}</p>
         <button id="break-increment" onClick={incrementBreakLengthByOneMinute}>+</button>
         <button id="break-decrement" onClick={decrementBreakLengthByOneMinute}>-</button>
     </div>

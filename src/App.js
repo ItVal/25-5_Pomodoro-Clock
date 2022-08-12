1a9f0b5ff622c346550ee4f4d198f55e8860814d
ItVal <valentinhonvj1@gmail.com>
2022-08-12 11:37:54 +0200

    refact component App, Session & break whith props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Break from './Components/Break';
 import Session from './Components/Session';
 
 function App() {
+     // initialisation de la durée de la pause à l'aide de react state (et useState)
+     const [sessionLengthInSeconds, setSessionLengthInSeconds] = useState(1500);
+// ajout bouton plus et moins avec des gestionnaires d'événements de clic
+  const decrementSessionLengthByOneMinute = () => {
+      const newSessionLengthInSeconds =
+      sessionLengthInSeconds - 60;
+      if (
+          newSessionLengthInSeconds < 0
+      ) {
+          setSessionLengthInSeconds(0);
+      } else {
+          setSessionLengthInSeconds(
+              newSessionLengthInSeconds
+          );
+      }
+  };
+  const incrementSessionLengthByOneMinute = () =>
+      setSessionLengthInSeconds(
+          sessionLengthInSeconds + 60
+      );
+
+ // initialisation de la durée de la pause à l'aide de react state (et useState)
+ const [breakLengthInSeconds, setBreakLengthInSeconds] = useState(300);
+// ajout bouton plus et moins avec des gestionnaires d'événements de clic
+const decrementBreakLengthByOneMinute = () => {
+  const newBreakLengthInSeconds =
+      breakLengthInSeconds - 60;
+  if (
+      newBreakLengthInSeconds < 0
+  ) {
+      setBreakLengthInSeconds(0);
+  } else {
+      setBreakLengthInSeconds(
+          newBreakLengthInSeconds
+      );
+  }
+};
+const incrementBreakLengthByOneMinute = () =>
+  setBreakLengthInSeconds(
+      breakLengthInSeconds + 60
+  );
+  
+
+  
   return (
     <div className="App">
-      <Break />
-      <Session />
+
+       {/* import de notre composant break avec l'accesoire props */}
+      <Break 
+      setBreakLengthInSeconds = {setBreakLengthInSeconds}
+      incrementBreakLengthByOneMinute = {incrementBreakLengthByOneMinute}
+      decrementBreakLengthByOneMinute = {decrementBreakLengthByOneMinute}
+      />
+
+      {/* import de notre composant session avec l'accesoire props */}
+      <Session 
+        setSessionLengthInSeconds = {setSessionLengthInSeconds}
+        incrementSessionLengthByOneMinute = {incrementSessionLengthByOneMinute}
+        decrementSessionLengthByOneMinute = {decrementSessionLengthByOneMinute}
+      
+      />
     </div>
   );
 }
--- src/Components/Break.jsx
+++ src/Components/Break.jsx
@@ -3,32 +3,16 @@ import React, { useState } from "react";
 
 
 
-const Break = () => {
-    // initialisation de la durée de la pause à l'aide de react state (et useState)
-    const [
-        breakLengthInSeconds,
-        setBreakLengthInSeconds
-    ] = useState(300);
-// ajout bouton plus et moins avec des gestionnaires d'événements de clic
-    const decrementBreakLengthByOneMinute = () => {
-        const newBreakLengthInSeconds =
-            breakLengthInSeconds - 60;
-        if (
-            newBreakLengthInSeconds < 0
-        ) {
-            setBreakLengthInSeconds(0);
-        } else {
-            setBreakLengthInSeconds(
-                newBreakLengthInSeconds
-            );
-        }
-    };
-    const incrementBreakLengthByOneMinute = () =>
-        setBreakLengthInSeconds(
-            breakLengthInSeconds + 60
-        );
-        // conversion secondes en minutes à l'aide de moment.js
-    const breakLengthInMinutes = moment.duration(breakLengthInSeconds, "s").minutes(); 
+const Break = ({
+    breakLengthInSeconds,
+    incrementBreakLengthByOneMinute,
+    decrementBreakLengthByOneMinute
+
+}) => {
+
+
+    // conversion secondes en minutes à l'aide de moment.js
+   const breakLengthInMinutes = moment.duration(breakLengthInSeconds, "s").minutes(); 
 return (
     <div>
         <p id="break-label">Break</p>

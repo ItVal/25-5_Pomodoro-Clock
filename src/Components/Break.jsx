8ca3e5d4fb3cc90b3cddf88b877fa3f61ae739a5
ItVal <valentinhonvj1@gmail.com>
2022-08-12 10:17:18 +0200

    feat : add plus and minus buttons with click event handlers

--- src/Components/Break.jsx
+++ src/Components/Break.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 
 
@@ -8,13 +8,30 @@ const Break = () => {
         setBreakLengthInSeconds
     ] = useState(300);
 
+    const decrementBreakLengthByOneMinute = () => {
+        const newBreakLengthInSeconds =
+            breakLengthInSeconds - 60;
+        if (
+            newBreakLengthInSeconds < 0
+        ) {
+            setBreakLengthInSeconds(0);
+        } else {
+            setBreakLengthInSeconds(
+                newBreakLengthInSeconds
+            );
+        }
+    };
+    const incrementBreakLengthByOneMinute = () =>
+        setBreakLengthInSeconds(
+            breakLengthInSeconds + 60
+        );
 
-    return (
-    <div> 
+return (
+    <div>
         <p id="break-label">Break</p>
         <p id="break-length">{breakLengthInSeconds}</p>
     </div>
-    );
+);
 };
 
 export default Break;

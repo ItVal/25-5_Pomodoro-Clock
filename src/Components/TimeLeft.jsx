39b37f2e66565d7833e4c319c307284aab34812e
ItVal <valentinhonvj1@gmail.com>
2022-08-12 12:05:40 +0200

    feat : install & branch moment duration format plugin

--- src/Components/TimeLeft.jsx
+++ src/Components/TimeLeft.jsx
@@ -1,5 +1,11 @@
+
+import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
 import React from 'react'
 import {useState} from 'react'
+
+// branchement du plugin moment duration pour le format mm:ss
+momentDurationFormatSetup(moment);
 
 function TimeLeft ({sessionLengthInSeconds}){
     const [timeLeft] = useState(sessionLengthInSeconds)
